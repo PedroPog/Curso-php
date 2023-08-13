5229ae52c23e14bf94d2eeb4e4bb05c94abc45a7
Pedro Vieira <pevieiraf@gmail.com>
2023-08-13 13:44:58 -0300

    alterações

--- ex01/nav.php
+++ ex01/nav.php
@@ -1,6 +1,5 @@
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
@@ -10,7 +9,6 @@
       </button>
       <a class="navbar-brand" href="#">Books on Line</a>
     </div>
-    <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class=""><a href="#">Home <span class="sr-only">(current)</span></a></li>
@@ -27,16 +25,16 @@
           </ul>
         </li>
       </ul>
-      <form class="navbar-form navbar-left" role="search">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#contato">Contato</a></li>
+        <li><a href="#log"><span class="glyphicon glyphicon-log-in"> Logon</a></li>
+      </ul>
+      <form class="navbar-form navbar-right" role="search">
         <div class="form-group">
           <input type="text" class="form-control" placeholder="Buscar...">
         </div>
         <button type="submit" class="btn btn-default">Pesquisar</button>
       </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#contato">Contato</a></li>
-        <li><a href="#log"><span class="glyphicon glyphicon-log-in"> Logon</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
+    </div>
+  </div>
 </nav>
---
+++ ex01/navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar bg-body-tertiary navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+      </div>
+    </div>    
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="#contato">Contato</a></li>
+        <li><a href="#log"><span class="glyphicon glyphicon-log-in"> Logon</a></li>
+      </ul>
+  </div>
+</nav>
